e7c99b5de61d95cfb6aef1cdd41ffc9745dceae9
Terry Zhao <i.terryzhao@gmail.com>
2016-06-28 22:42:04 +0800

    get supervisor&gunicorn done

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!/user/bin/python
 from app import app
-app.run(debug = True, host="0.0.0.0")
+#app.run(debug = True, host="0.0.0.0", port=80)
+#app.run()
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
